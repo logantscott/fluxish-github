d5918c4d1e7a880fc41691b712de0fd0f9c98111
Logan Scott <logantscott@gmail.com>
2020-08-05 09:07:55 -0600

    base components

---
+++ src/components/github/UserRepos.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UserRepos = () => {
+  const repositories = () => {
+    return;
+  };
+
+  return (
+    <ul>
+      {repositories}
+    </ul>
+  );
+};
+
+export default UserRepos;
